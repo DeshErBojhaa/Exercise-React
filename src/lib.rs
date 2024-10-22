8695dc1c317e0ad766a3ff4526420d88233a1202
tamjidahmed <mail.to.tamjid@gmail.com>
2024-10-22 17:58:45 +0600

    v0.1.0
    Signed-off-by: tamjidahmed <mail.to.tamjid@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -47,32 +47,17 @@ pub struct Reactor<'a, T> {
     compute: HashMap<CellId, Box<ComputeCell<'a, T>>>,
 }
 
-// You are guaranteed that Reactor will only be tested against types that are Copy + PartialEq.
 impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
     pub fn new() -> Self {
         Self { inputs: HashMap::new(), compute: HashMap::new() }
     }
-
-    // Creates an input cell with the specified initial value, returning its ID.
+    
     pub fn create_input(&mut self, _initial: T) -> InputCellId {
         let id = InputCellId(self.inputs.len());
         self.inputs.insert(CellId::Input(id), Box::new(InputCell::new(_initial,vec![])));
         id
     }
-
-    // Creates a compute cell with the specified dependencies and compute function.
-    // The compute function is expected to take in its arguments in the same order as specified in
-    // `dependencies`.
-    // You do not need to reject compute functions that expect more arguments than there are
-    // dependencies (how would you check for this, anyway?).
-    //
-    // If any dependency doesn't exist, returns an Err with that nonexistent dependency.
-    // (If multiple dependencies do not exist, exactly which one is returned is not defined and
-    // will not be tested)
-    //
-    // Notice that there is no way to *remove* a cell.
-    // This means that you may assume, without checking, that if the dependencies exist at creation
-    // time they will continue to exist as long as the Reactor exists.
+    
     pub fn create_compute<F: Fn(&[T]) -> T + 'a>(
         &mut self,
         _dependencies: &[CellId],
@@ -120,95 +105,70 @@ impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
         }
         Ok(id)
     }
-
-    // Retrieves the current value of the cell, or None if the cell does not exist.
-    //
-    // You may wonder whether it is possible to implement `get(&self, id: CellId) -> Option<&Cell>`
-    // and have a `value(&self)` method on `Cell`.
-    //
-    // It turns out this introduces a significant amount of extra complexity to this exercise.
-    // We chose not to cover this here, since this exercise is probably enough work as-is.
+    
     pub fn value(&self, id: CellId) -> Option<T> {
         match id {
             CellId::Compute(_) => self.compute.get(&id).map(|c| c.val),
             CellId::Input(_) => self.inputs.get(&id).map(|c| c.val)
         }
     }
-
-    // Sets the value of the specified input cell.
-    //
-    // Returns false if the cell does not exist.
-    //
-    // Similarly, you may wonder about `get_mut(&mut self, id: CellId) -> Option<&mut Cell>`, with
-    // a `set_value(&mut self, new_value: T)` method on `Cell`.
-    //
-    // As before, that turned out to add too much extra complexity.
+    
     pub fn set_value(&mut self, _id: InputCellId, _new_value: T) -> bool {
         let _id = CellId::Input(_id);
         if !self.inputs.contains_key(&_id) {
             return false;
         }
-        let mut children: Vec<CellId> = Vec::new();
+        
         if let Some(c) = self.inputs.get_mut(&_id) { 
             c.val = _new_value;
-            for child in c.children.iter() {
-                children.push(*child);
-            }
+            let mut topo: Vec<CellId> = Vec::new();
+            let mut seen: HashSet<CellId> = HashSet::new();
+            self.get_topo_order(_id, &mut topo, &mut seen);
+            
+            topo.reverse();
+            self.update_compute_cell_value(&topo[1..]);
         }
-        let mut callbacks_cells: HashSet<CellId> = HashSet::new();
-        for child in children {
-            self.update_compute_cell_value(&child, &mut callbacks_cells);    
-        }
-        
-        for id in callbacks_cells {
-            let cell = self.compute.get_mut(&id).unwrap();
-            for cb in cell.callbacks.values_mut() {
-                (cb)(cell.val);
-            }
-        }
-        
         true
     }
-
-    fn update_compute_cell_value(&mut self, id: &CellId, callbacks_cells: &mut HashSet<CellId>) {
-        let parent_values: Vec<T> = self
-            .compute
-            .get(id)
-            .unwrap()
-            .parents
-            .iter()
-            .map(|par| self.value(*par).unwrap())
-            .collect();
-
-        // Now it's safe to mutate `self` since we no longer need immutable borrows
-        let cell = self.compute.get_mut(id).unwrap();
-        let new_val = (cell.func)(&parent_values);
-
-        if new_val != cell.val {
-            cell.val = new_val;
-            callbacks_cells.insert(*id);
+    
+    fn get_topo_order(&mut self, id: CellId, stack: &mut Vec<CellId>, seen: &mut HashSet<CellId>) {
+        seen.insert(id);
+        let children = match id {
+            CellId::Input(_) => {self.inputs.get_mut(&id).unwrap().children.clone()}
+            CellId::Compute(_) => {self.compute.get_mut(&id).unwrap().children.clone()}
+        };
+        for cid in children {
+            if seen.contains(&cid) {
+                continue;
+            }
+            self.get_topo_order(cid, stack, seen);
         }
-        
-        let children: Vec<CellId> = cell.children.clone();
-        
-        for child in children {
-            self.update_compute_cell_value(&child, callbacks_cells);
-        }
+        stack.push(id);
     }
 
+    fn update_compute_cell_value(&mut self, queue: &[CellId]) {
+        for cell_id in queue {
+            let parent_values: Vec<T> = self
+                .compute
+                .get(cell_id)
+                .unwrap()
+                .parents
+                .iter()
+                .map(|par| self.value(*par).unwrap())
+                .collect();
+            
+            let cell = self.compute.get_mut(cell_id).unwrap();
+            let new_val = (cell.func)(&parent_values);
 
-    // Adds a callback to the specified compute cell.
-    //
-    // Returns the ID of the just-added callback, or None if the cell doesn't exist.
-    //
-    // Callbacks on input cells will not be tested.
-    //
-    // The semantics of callbacks (as will be tested):
-    // For a single set_value call, each compute cell's callbacks should each be called:
-    // * Zero times if the compute cell's value did not change as a result of the set_value call.
-    // * Exactly once if the compute cell's value changed as a result of the set_value call.
-    //   The value passed to the callback should be the final value of the compute cell after the
-    //   set_value call.
+            if new_val != cell.val {
+                cell.val = new_val;
+                for cb in cell.callbacks.values_mut() {
+                    (cb)(new_val);    
+                }
+            }
+        }
+    }
+    
     pub fn add_callback<F: 'a + FnMut(T)>(
         &mut self,
         _id: ComputeCellId,
@@ -224,12 +184,7 @@ impl<'a, T: Copy + PartialEq> Reactor<'a, T> {
             }
         }
     }
-
-    // Removes the specified callback, using an ID returned from add_callback.
-    //
-    // Returns an Err if either the cell or callback does not exist.
-    //
-    // A removed callback should no longer be called.
+    
     pub fn remove_callback(
         &mut self,
         cell: ComputeCellId,
